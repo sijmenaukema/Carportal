4468c9f443e26069d20d3b4bb3c5f95b1c29393a
Aukema, S (Sijmen) <sijmen.aukema@rabobank.nl>
2020-03-16 13:32:30 +0100

    first

---
+++ Carportal/src/main/java/adessoassessment/carportal/CarportalApplication.java
@@ -0,0 +1,13 @@
+package adessoassessment.carportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarportalApplication.class, args);
+	}
+
+}
---
+++ Carportal/src/test/java/adessoassessment/carportal/CarportalApplicationTests.java
@@ -0,0 +1,13 @@
+package adessoassessment.carportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
