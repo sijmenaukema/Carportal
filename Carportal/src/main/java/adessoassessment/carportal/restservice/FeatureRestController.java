aefc05b26fca8228f12c069a43d73aec4c935f06
Aukema, S (Sijmen) <sijmen.aukema@rabobank.nl>
2020-03-18 11:36:27 +0100

    imported ResourceNotFoundException

--- Carportal/src/main/java/adessoassessment/carportal/restservice/FeatureRestController.java
+++ Carportal/src/main/java/adessoassessment/carportal/restservice/FeatureRestController.java
@@ -1,8 +1,11 @@
 package adessoassessment.carportal.restservice;
 
 
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +19,17 @@ public class FeatureRestController {
     private FeatureRepository featureRepository;
 
     @GetMapping("/features")
-    public List<Features> getAllFeatures(){
+    public List<Features> getAllFeatures() {
         return featureRepository.findAll() ;
     }
+    @GetMapping("/features/{id}")
+    public ResponseEntity<Features> getNameById(@PathVariable(value = "id") Long id)
+        throws ResourceNotFoundException {
+        Features features =
+                featureRepository
+                        .findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("No model is found on :: " + id));
+        return ResponseEntity.ok().body(features);
+    }
+
 }
