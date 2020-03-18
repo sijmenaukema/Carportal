58fcd60f151f68f2f5254c562901c6ab2b970443
Aukema, S (Sijmen) <sijmen.aukema@rabobank.nl>
2020-03-18 18:21:40 +0100

    EINDE

--- Carportal/src/main/java/adessoassessment/carportal/restservice/Features.java
+++ Carportal/src/main/java/adessoassessment/carportal/restservice/Features.java
@@ -1,13 +1,14 @@
 package adessoassessment.carportal.restservice;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "features")
